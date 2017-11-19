c80232a231be6e09dbac1a2185fa20096576da9e
Ockenheimer <markusschaefer85@gmx.de>
2017-11-19 17:15:54 +0100

    Abgabereif

--- src/ai801_aufgaben/Aufgabe2.java
+++ src/ai801_aufgaben/Aufgabe2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ai801_aufgaben;
 
-/**
- *
- * @author studentai
- */
+
 public class Aufgabe2 {
 
     public static int summe(int grenze) {
--- src/ai801_aufgaben/Aufgabe3.java
+++ src/ai801_aufgaben/Aufgabe3.java
@@ -1,29 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ai801_aufgaben;
 
-/**
- *
- * @author studentai
- */
 public class Aufgabe3 {
 
     public static long summe(int grenze) {
         int an1 = 1;
         int an = 2;
         int merk;
-        //int an;
 
         long summe = 0;
-        while (an <= grenze){
+        while (an <= grenze) {
 
             if (an % 2 == 0) {
                 summe += an;
             }
-            merk=an;
+            merk = an;
             an += an1;
             an1 = merk;
 
