0af5df3e0c8eaa4ce44388ebdef79b096ee44239
Ockenheimer <markusschaefer85@gmx.de>
2017-11-19 10:37:02 +0100

    HEUREKA

--- src/ai801_aufgaben/AI801_Aufgaben.java
+++ src/ai801_aufgaben/AI801_Aufgaben.java
@@ -17,6 +17,8 @@ public class AI801_Aufgaben {
     public static void main(String[] args) {
         System.out.println(Aufgabe2.summe(1000));
         System.out.println(Aufgabe3.summe(4000000));
+        System.out.println(Aufgabe4.summe(10000));
+        //       System.out.println(friendlyNumber.friendlyNumber(285));
     }
 
 }
