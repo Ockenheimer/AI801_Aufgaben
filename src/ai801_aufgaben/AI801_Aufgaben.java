455bc6681c974bba9b508a74847b34ddb1a2c850
Ockenheimer <markusschaefer85@gmx.de>
2017-11-19 17:15:27 +0100

    Abgabereif

--- src/ai801_aufgaben/AI801_Aufgaben.java
+++ src/ai801_aufgaben/AI801_Aufgaben.java
@@ -1,24 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ai801_aufgaben;
 
-/**
- *
- * @author studentai
- */
+
 public class AI801_Aufgaben {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         System.out.println(Aufgabe2.summe(1000));
         System.out.println(Aufgabe3.summe(4000000));
         System.out.println(Aufgabe4.summe(10000));
-        //       System.out.println(friendlyNumber.friendlyNumber(285));
+    
     }
 
 }
