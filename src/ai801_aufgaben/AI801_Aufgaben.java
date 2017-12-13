2fe407056bf725eb29c80eea0722e0b193e12369
Ockenheimer <markusschaefer85@gmx.de>
2017-12-13 09:10:13 +0100

    AI 801 Abgeschlossen

--- src/ai801_aufgaben/AI801_Aufgaben.java
+++ src/ai801_aufgaben/AI801_Aufgaben.java
@@ -13,16 +13,10 @@ public class AI801_Aufgaben {
             b6.hinzufuegen(array[i]);
             
         }
+        System.out.println(b6.root.toString());
         
-        System.out.println(b6.suchen('s', b6.root));
-        System.out.println(b6.suchen('c', b6.root));
-        System.out.println(b6.suchen('h', b6.root));
-        System.out.println(b6.suchen('a', b6.root));
-        System.out.println(b6.suchen('e', b6.root));
-        System.out.println(b6.suchen('f', b6.root));
-        System.out.println(b6.suchen('e', b6.root));
-        System.out.println(b6.suchen('r', b6.root));
-        
+        b6.inOrder(b6.root);
+  
     }
 
 }
